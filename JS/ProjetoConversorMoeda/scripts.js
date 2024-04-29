c1b110461dfacf5cd7a9c26aefb0acbfaca33ee0
cleiton lahm <cleijllahm@gmail.com>
2024-04-29 07:35:25 -0300

    adicionando os valores de pre e pos conversao

--- JS/ProjetoConversorMoeda/scripts.js
+++ JS/ProjetoConversorMoeda/scripts.js
@@ -1,12 +1,20 @@
 const convertButton = document.querySelector(".convert-button")
 const inputValue = document.querySelector(".input-value")
 
+
 function convertValues() {
   const inputValue = document.querySelector(".input-value").value
-  const dolarToday = 5.2
+  const initialValue = document.querySelector(".currency-value-initial")
+  const finalValue = document.querySelector(".currency-value-final")
   
+  const dolarToday = 5.2
   const convertedValue = inputValue / dolarToday
-  console.log(convertedValue)
+
+  initialValue.innerHTML = inputValue
+  finalValue.innerHTML = convertedValue
+  
+
 }
 
 convertButton.addEventListener('click', convertValues)
+
