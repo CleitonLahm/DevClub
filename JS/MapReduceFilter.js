9cbb79e2f12f05e67f01da82ea226ca72d2a39dd
cleiton lahm <cleijllahm@gmail.com>
2024-05-16 11:42:33 -0300

    map reduce e filter

---
+++ JS/MapReduceFilter.js
@@ -0,0 +1,27 @@
+const companies = [
+  {name: 'Samsung', marketValue: 50, CEO: 'kim Hyum Suk', foundedOn: 1938},
+  {name: 'Microsoft', marketValue: 415, CEO: 'Satya Nadella', foundedOn: 1975},
+  {name: 'Intel', marketValue: 117, CEO: 'Brian Krzanich', foundedOn: 1968},
+  {name: 'Facebook', marketValue: 383, CEO: 'Mark Zuckenberg', foundedOn: 2004},
+  {name: 'Spotify', marketValue: 30, CEO: 'Daniel Ek', foundedOn: 2006},
+  {name: 'Apple', marketValue: 845, CEO: 'Tim Cook', foundedOn: 1976},
+]
+
+const companiesAtt = companies.map( companie => {
+  const newMarketValue = (companie.marketValue * 1.1).toFixed(2)
+  return {...companie, marketValue: parseFloat(newMarketValue)}
+})
+
+const companiesFoundedBeforeNinety = companiesAtt.filter(companie => {
+  return companie.foundedOn < 1990
+})
+
+const totalValueCompaniesBeforeNinety = companiesFoundedBeforeNinety.reduce((acc, value) => {
+  return acc + value.marketValue
+}, 0).toFixed(2)
+
+
+
+
+console.log(`O valor de mercado das empresas foi atualizado e o filtro pegando empresas fundadas antes de 1990 foi aplicado, somando todo o valor de mercado dessas empresas filtradas chegamos ao valor de R$ ${totalValueCompaniesBeforeNinety}`)
+
