7021592eaece838baa86380235ce97b1b6173b60
cleiton lahm <cleijllahm@gmail.com>
2024-05-18 09:32:51 -0300

    funcoes com foreach, map e reduce feitas

--- JS/ProjetoCardapio/scripts.js
+++ JS/ProjetoCardapio/scripts.js
@@ -1,12 +1,13 @@
 const list = document.querySelector("ul");
-let myLi = "";
 const buttonShowAll = document.querySelector(".show-all");
+const mapAll = document.querySelector(".map-all");
+const sumAll = document.querySelector(".sum-all");
+const divResult = document.querySelector('.result')
 
-function showAll() {
-  menuOptions.forEach((product) => {
-    myLi =
-      myLi +
-      `
+function showAll(productsArray) {
+  let myLi = "";
+  productsArray.forEach((product) => {
+    myLi += `
     <li>
       <img src=${product.src}>
       <p>${product.name}</p>
@@ -18,4 +19,29 @@ function showAll() {
   });
 }
 
-buttonShowAll.addEventListener("click", showAll);
+function mapAllItems() {
+  const newPrices = menuOptions.map((product) => ({
+    ...product,
+    price: (product.price * 0.9).toFixed(2),
+  }));
+  showAll(newPrices);
+}
+
+function sumAllMenu() {
+  list.innerHTML = ''
+
+  const menuTotalValue = menuOptions.reduce((acc, product) => {
+    return acc + product.price
+  }, 0) 
+  divResult.textContent = `Total Value: $${menuTotalValue.toFixed(2)}`;
+
+
+  console.log(`Total Value: ${menuTotalValue}`);
+}
+
+
+
+
+buttonShowAll.addEventListener("click", () => showAll(menuOptions));
+mapAll.addEventListener("click", mapAllItems);
+sumAll.addEventListener('click', sumAllMenu)
