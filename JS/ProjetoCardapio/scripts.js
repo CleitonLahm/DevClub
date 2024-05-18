bdb83879e2b453b67ab4546cc61d9dab65f7b5da
cleiton lahm <cleijllahm@gmail.com>
2024-05-18 09:57:48 -0300

    filter, projeto finalizado

--- JS/ProjetoCardapio/scripts.js
+++ JS/ProjetoCardapio/scripts.js
@@ -3,6 +3,15 @@ const buttonShowAll = document.querySelector(".show-all");
 const mapAll = document.querySelector(".map-all");
 const sumAll = document.querySelector(".sum-all");
 const divResult = document.querySelector(".result");
+const veganFilter = document.querySelector(".vegan-filter");
+
+function formatCurrency(value) {
+  const newValue = value.toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  });
+  return newValue
+}
 
 function showAll(productsArray) {
   divResult.textContent = "";
@@ -12,7 +21,7 @@ function showAll(productsArray) {
     <li>
       <img src=${product.src}>
       <p>${product.name}</p>
-      <p class="item-price">${product.price}</p>
+      <p class="item-price">${formatCurrency(product.price)}</p>
   </li>
     `;
 
@@ -23,7 +32,7 @@ function showAll(productsArray) {
 function mapAllItems() {
   const newPrices = menuOptions.map((product) => ({
     ...product,
-    price: (product.price * 0.9).toFixed(2),
+    price: formatCurrency((product.price * 0.9).toFixed(2)),
   }));
   showAll(newPrices);
 }
@@ -34,9 +43,15 @@ function sumAllMenu() {
   const menuTotalValue = menuOptions.reduce((acc, product) => {
     return acc + product.price;
   }, 0);
-  divResult.textContent = `Total Value: R$ ${menuTotalValue.toFixed(2)}`;
+  divResult.textContent = `Total Value: R$ ${formatCurrency(menuTotalValue.toFixed(2))}`;
+}
+
+function veganFilterItems() {
+  const VeganProducts = menuOptions.filter((product) => product.vegan);
+  showAll(VeganProducts);
 }
 
 buttonShowAll.addEventListener("click", () => showAll(menuOptions));
 mapAll.addEventListener("click", mapAllItems);
 sumAll.addEventListener("click", sumAllMenu);
+veganFilter.addEventListener("click", veganFilterItems);
